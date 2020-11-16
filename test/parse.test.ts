b4ccb5381c027f2c81e05d0429813c888710953b
Nikos Kostoulas <nikostoulas@users.noreply.github.com>
2020-11-16 12:44:44 +0200

    Revamp matching algorithm (#10)
    * Revamp matching algorithm
    
    * fixup! Revamp matching algorithm
    
    * Update github actions

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -5,65 +5,65 @@ import { routes as routes2 } from './routes.paths';
 import { routes as routes3 } from './routes.backslash';
 import { parse, handlePathVariables, getPathMethod, addPrefixMiddleware } from '../src/parse';
 
-describe('Parse', function() {
-  it('output should be the same with different ways of routes', function() {
-    assert.deepEqual(parse(routes), parse(routes1));
-    assert.deepEqual(parse(routes), parse(routes2));
+describe('Parse', function () {
+  it('output should be the same with different ways of routes', function () {
+    assert.deepStrictEqual(parse(routes), parse(routes1));
+    assert.deepStrictEqual(parse(routes), parse(routes2));
   });
 
-  it('should handle path variables', function() {
+  it('should handle path variables', function () {
     snapshot(handlePathVariables(parse(routes)));
   });
 
-  it('should remove ending slash', function() {
+  it('should remove ending slash', function () {
     snapshot(handlePathVariables(parse(routes3)));
   });
 
-  it('should return routes for get', function() {
-    assert.deepEqual(getPathMethod(handlePathVariables(parse(routes)), '/path', 'get'), {
+  it('should return routes for get', function () {
+    assert.deepStrictEqual(getPathMethod(handlePathVariables(parse(routes)), '/path', 'get'), {
       _matchedRoute: '/path',
       middleware: []
     });
   });
 
-  it('should return routes for policy and path with params', function() {
-    assert.deepEqual(getPathMethod(handlePathVariables(parse(routes)), '/nested/path/id', 'policy'), {
+  it('should return routes for policy and path with params', function () {
+    assert.deepStrictEqual(getPathMethod(handlePathVariables(parse(routes)), '/nested/path/id', 'policy'), {
       middleware: [],
       params: { id: 'id' },
       _matchedRoute: '/nested/path/:id'
     });
   });
 
-  it('should return routes ignoring ending slash', function() {
-    assert.deepEqual(getPathMethod(handlePathVariables(parse(routes)), '/nested/path/id/', 'policy'), {
+  it('should return routes ignoring ending slash', function () {
+    assert.deepStrictEqual(getPathMethod(handlePathVariables(parse(routes)), '/nested/path/id/', 'policy'), {
       middleware: [],
       params: { id: 'id' },
       _matchedRoute: '/nested/path/:id'
     });
   });
 
-  it('should return home route ignoring ending slash', function() {
-    assert.deepEqual(getPathMethod(handlePathVariables(parse(routes3)), '/', 'get'), {
+  it('should return home route ignoring ending slash', function () {
+    assert.deepStrictEqual(getPathMethod(handlePathVariables(parse(routes3)), '/', 'get'), {
       middleware: [],
       _matchedRoute: ''
     });
   });
 
-  it('should return no route if path does not match', function() {
-    assert.deepEqual(getPathMethod(handlePathVariables(parse(routes)), '/nested/path/id/foo', 'policy'), {});
+  it('should return no route if path does not match', function () {
+    assert.deepStrictEqual(getPathMethod(handlePathVariables(parse(routes)), '/nested/path/id/foo', 'policy'), {});
   });
 
-  describe('addPrefixMiddleware', function() {
-    it('should handle path variables', function() {
+  describe('addPrefixMiddleware', function () {
+    it('should handle path variables', function () {
       snapshot(handlePathVariables(addPrefixMiddleware(parse(routes), routes.prefix)));
     });
 
-    it('should remove ending slash', function() {
+    it('should remove ending slash', function () {
       snapshot(handlePathVariables(addPrefixMiddleware(parse(routes3), routes3.prefix)));
     });
 
-    it('should return routes for get', function() {
-      assert.deepEqual(
+    it('should return routes for get', function () {
+      assert.deepStrictEqual(
         getPathMethod(handlePathVariables(addPrefixMiddleware(parse(routes), routes.prefix)), '/path', 'get'),
         {
           _matchedRoute: '/path',
@@ -72,8 +72,8 @@ describe('Parse', function() {
       );
     });
 
-    it('should not add if prefix ends with /', function() {
-      assert.deepEqual(
+    it('should not add if prefix ends with /', function () {
+      assert.deepStrictEqual(
         getPathMethod(
           handlePathVariables(
             addPrefixMiddleware(
@@ -96,8 +96,8 @@ describe('Parse', function() {
     });
 
     // tslint:disable-next-line: quotemark
-    it("should add prefix if it doesn't end with /", function() {
-      assert.deepEqual(
+    it("should add prefix if it doesn't end with /", function () {
+      assert.deepStrictEqual(
         getPathMethod(
           handlePathVariables(
             addPrefixMiddleware(
@@ -119,7 +119,7 @@ describe('Parse', function() {
       );
     });
 
-    it('should add prefix middleware to all matching routes', function() {
+    it('should add prefix middleware to all matching routes', function () {
       snapshot(
         addPrefixMiddleware(
           {
@@ -145,8 +145,95 @@ describe('Parse', function() {
     });
   });
 
-  describe('route that matches every path', function() {
-    it('should fallback to star routes', function() {
+  describe('route that matches every path', function () {
+    it('should fallback to star with path variable', function () {
+      let routes = handlePathVariables(
+        addPrefixMiddleware(
+          parse({
+            get: {
+              '/path': ['middleware'],
+              '/path/1': ['one'],
+              '/path/1/3/foo': ['foo'],
+              '/:path/*': ['star'],
+              '/*': ['initialStar']
+            }
+          }),
+          { '/path': 'prefix' }
+        )
+      );
+
+      assert.deepStrictEqual(getPathMethod(routes, '/path/foo', 'get'), {
+        _matchedRoute: '/:path/*',
+        params: { path: 'path' },
+        middleware: ['star']
+      });
+    });
+
+    it('star path should also match with path ending in /', function () {
+      let routes = handlePathVariables(
+        addPrefixMiddleware(
+          parse({
+            get: {
+              '/path': ['middleware'],
+              '/path/1': ['one'],
+              '/path/1/3/foo': ['foo'],
+              '/:path/*': ['star'],
+              '/*': ['initialStar']
+            }
+          }),
+          { '/path': 'prefix' }
+        )
+      );
+
+      assert.deepStrictEqual(getPathMethod(routes, '/foo/', 'get'), {
+        _matchedRoute: '/:path/*',
+        params: { path: 'foo' },
+        middleware: ['star']
+      });
+
+      assert.deepStrictEqual(getPathMethod(routes, '/foo', 'get'), {
+        _matchedRoute: '/:path/*',
+        params: { path: 'foo' },
+        middleware: ['star']
+      });
+    });
+
+    it('star path should also work with many params in path', function () {
+      let routes = handlePathVariables(
+        addPrefixMiddleware(
+          parse({
+            get: {
+              '/path': ['middleware'],
+              '/path/1': ['one'],
+              '/path/1/3/foo': ['foo'],
+              '/:path/:value/*': ['star'],
+              '/*': ['initialStar']
+            }
+          }),
+          { '/path': 'prefix' }
+        )
+      );
+
+      assert.deepStrictEqual(getPathMethod(routes, '/path/2/3', 'get'), {
+        _matchedRoute: '/:path/:value/*',
+        params: { path: 'path', value: '2' },
+        middleware: ['star']
+      });
+
+      assert.deepStrictEqual(getPathMethod(routes, '/path/1/3', 'get'), {
+        _matchedRoute: '/:path/:value/*',
+        params: { path: 'path', value: '1' },
+        middleware: ['star']
+      });
+
+      assert.deepStrictEqual(getPathMethod(routes, '/path/1/3/foo/bar', 'get'), {
+        _matchedRoute: '/:path/:value/*',
+        params: { path: 'path', value: '1' },
+        middleware: ['star']
+      });
+    });
+
+    it('should fallback to star routes', function () {
       let routes = handlePathVariables(
         addPrefixMiddleware(
           parse({
@@ -161,34 +248,37 @@ describe('Parse', function() {
           { '/path': 'prefix' }
         )
       );
-      assert.deepEqual(getPathMethod(routes, '/path/1', 'get'), {
+      assert.deepStrictEqual(getPathMethod(routes, '/path/1', 'get'), {
         _matchedRoute: '/path/1',
         middleware: ['prefix', 'one']
       });
 
-      assert.deepEqual(getPathMethod(routes, '/path/foo', 'get'), {
+      assert.deepStrictEqual(getPathMethod(routes, '/path/foo', 'get'), {
         _matchedRoute: '/path/*',
+        params: {},
         middleware: ['prefix', 'star']
       });
 
-      assert.deepEqual(getPathMethod(routes, '/path/1/3', 'get'), {
+      assert.deepStrictEqual(getPathMethod(routes, '/path/1/3', 'get'), {
         _matchedRoute: '/path/*',
+        params: {},
         middleware: ['prefix', 'star']
       });
 
-      assert.deepEqual(getPathMethod(routes, '/path/1/2', 'get'), {
+      assert.deepStrictEqual(getPathMethod(routes, '/path/1/2', 'get'), {
         _matchedRoute: '/path/*',
+        params: {},
         middleware: ['prefix', 'star']
       });
 
-      assert.deepEqual(getPathMethod(routes, '/', 'get'), {
+      assert.deepStrictEqual(getPathMethod(routes, '/', 'get'), {
         _matchedRoute: '/*',
         middleware: ['initialStar'],
         params: {}
       });
     });
 
-    it('should fallback to star routes', function() {
+    it('should fallback to star routes', function () {
       let routes = handlePathVariables(
         addPrefixMiddleware(
           parse({
@@ -204,7 +294,7 @@ describe('Parse', function() {
         )
       );
 
-      assert.deepEqual(getPathMethod(routes, '/path2/1', 'get'), {});
+      assert.deepStrictEqual(getPathMethod(routes, '/path2/1', 'get'), {});
     });
   });
 });
